c9c6a41523135fa8c56ebd701db2aaa4bfd92cd0
Bilal Ahmad <bilal.ahmad@focusteck.com>
2024-01-31 19:28:44 +0500

    initial commit server created

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require('cookie-parser');
-const { PORT } = require("./src/config/config")
+const { PORT } = require("./src/dbConfig/config")
 
 const app = express();
 
